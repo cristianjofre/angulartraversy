977b4588c2e7c61919b618f8846cc54fb287b63d
cristianjofre <megalocoman@gmail.com>
2021-11-14 23:05:48 -0300

    first commit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  title : string = 'task tracker';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleAddTask(){
+
+  }
+
+}
